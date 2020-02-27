58a64b10590d6de2947720fd4a9de28a355421d0
Samuel Ortiz <kevinsamuel398@gmail.com>
2020-02-27 18:46:20 -0500

    Creación Clase

--- potencia.py
+++ potencia.py
@@ -1,1 +1,13 @@
+class Potencia():
+    def __init__(self, cadena, potencia, tipo):
+        self.caracteres = cadena
+        self.letras = cadena
+        self.potencia = potencia
+        self.tipo = tipo
 
+    def alfa(self):
+        lista = []
+        for i in range(0, len(self.caracteres)):
+            for j in range(0, len(self.letras)):
+                lista.append(self.caracteres[i], self.letras[j])
+        self.letras = lista
