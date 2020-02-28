3106f6bb70fa86ac0cdbe533b6f256cf65c1b847
Luis Puello <luiscapupa@gmail.com>
2020-02-27 19:18:58 -0500

    potencia y vista

--- potencia.py
+++ potencia.py
@@ -11,3 +11,15 @@ class Potencia():
             for j in range(0, len(self.letras)):
                 lista.append(self.caracteres[i], self.letras[j])
         self.letras = lista
+        
+
+    def verificar(self):
+        if self.tipo == "-vd":
+            print(self.alfa())
+        elif self.tipo == "+vd":
+            print(self.paso_a_paso())
+    
+    def paso_a_paso(self):
+        pass
+    
+    
--- vista.py
+++ vista.py
@@ -1,8 +1,11 @@
 import sys 
 from potencia import Potencia
+print(sys.argv)
 user = sys.argv[1]
 pot = sys.argv[2]
 detalle = sys.argv[3]
+user = {i for i in user.replace('{','').replace('}','').split(',')}
+pot = {i for i in pot}
 print (user)
 print (pot)
 print (detalle)
